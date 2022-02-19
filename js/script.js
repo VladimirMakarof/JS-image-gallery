00f16acff7762b4993ac26fdc0e6e1139adf242e
VladimirMakarof <stan13.09@mail.ru>
2022-02-19 10:40:47 +0300

    initializing variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
+const search = document.querySelector('.fa-search');
+const input = document.getElementById('input');
+const grid = document.getElementById('grid');
 
+const key = 'EuAqDWSjvx_5xaYeLp7nQ1W53JBUeBPWw9d8m4eE7io';
