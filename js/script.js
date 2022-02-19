48c2503d4b71ca038488c97d21566b635e0a2f02
VladimirMakarof <stan13.09@mail.ru>
2022-02-19 10:42:16 +0300

    feat: add dayNightTheme

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ const input = document.getElementById('input');
 const grid = document.getElementById('grid');
 
 const key = 'EuAqDWSjvx_5xaYeLp7nQ1W53JBUeBPWw9d8m4eE7io';
+
+// светлая и темная тема
+
+dayNightTheme = () => {
+    let date = new Date();
+    let hour = date.getHours();
+
+    if (hour >= 7 && hour < 19) {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+    else {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    }
+}
